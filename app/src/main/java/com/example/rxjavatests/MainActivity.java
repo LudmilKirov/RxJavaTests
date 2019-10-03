9ce4877458a871d178d2ec4c2633bd17b02b844e
LudmilKirov <ludmil9595@gmail.com>
2019-10-03 16:23:09 +0300

    Add a more complex case

--- app/src/main/java/com/example/rxjavatests/MainActivity.java
+++ app/src/main/java/com/example/rxjavatests/MainActivity.java
@@ -5,55 +5,79 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private Disposable disposable;
+    private CompositeDisposable mCompositeDisposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Observable
-        Observable<String> animalsObservable = getAnimalsObservable();
-
-        //Observer
-        Observer<String> animalsObserver = getAnimalsObserver();
-
-        //Observer subscribing to observable
-        animalsObservable
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .filter(new Predicate<String>() {
-                    @Override
-                    public boolean test(String s) throws Exception {
-                        return s.toLowerCase().startsWith("b");
-                    }
-                })
-                .subscribeWith(animalsObserver);
+        //add to composite observable
+        //.map() operator  is used to turn the note into all uppercase letters
+        mCompositeDisposable.add(getNotesObservable()
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .map(new Function<Note, Note>() {
+            @Override
+            public Note apply(Note note) throws Exception {
+                //Making the note all uppercase
+                note.setNote(note.getNote().toUpperCase());
+                return note;
+            }
+        }).subscribeWith(getNotesObserver()));
     }
 
-    private Observer<String> getAnimalsObserver() {
-        return new Observer<String>() {
+    private List<Note> prepareNotes() {
+        List<Note> notes = new ArrayList<>();
+        notes.add(new Note(1, "buy beer"));
+        notes.add(new Note(2, "buy vodka"));
+        notes.add(new Note(3, "buy whiskey"));
+        return notes;
+    }
 
+    private Observable<Note> getNotesObservable() {
+        final List<Note> notes = prepareNotes();
+
+        return Observable.create(new ObservableOnSubscribe<Note>() {
             @Override
-            public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe: ");
-                disposable=d;
+            public void subscribe(ObservableEmitter<Note> emitter) throws Exception {
+                for (Note note : notes) {
+                    if (!emitter.isDisposed()) {
+                        emitter.onNext(note);
+                    }
+                }
+
+                if (!emitter.isDisposed()) {
+                    emitter.onComplete();
+                }
             }
+        });
+    }
 
+    private DisposableObserver<Note> getNotesObserver() {
+        return new DisposableObserver<Note>() {
             @Override
-            public void onNext(String s) {
-                Log.d(TAG, "Name: " + s);
+            public void onNext(Note note) {
+                Log.d(TAG, "onNext: " + note.getNote());
             }
 
             @Override
@@ -68,16 +92,10 @@ public class MainActivity extends AppCompatActivity {
         };
     }
 
-
-    private Observable<String> getAnimalsObservable() {
-        return Observable.fromArray("Ant", "Bee", "Cat", "Dog", "Fox","Bat","Bee","Bear","Crab","Cod","Fox");
-    }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-        //dont send events once the activity is destroyed
-        disposable.dispose();
+        mCompositeDisposable.clear();
     }
 }
+
